4b14d9f2caf4978b60657716515f9c5e946faa6f
Olesya Kurilenko <olesya.kurilenko@gmail.com>
2018-03-30 12:26:06 +0200

    correction hw16

--- src/Components/ComUserTasksInfo.js
+++ src/Components/ComUserTasksInfo.js
@@ -3,23 +3,21 @@ export class ComUserTasksInfo extends Component {
     userId = 1, name = 'UserName', done = 1, inProgress = 1, waiting = 1
   }) {
     super();
-    this.userId = userId;
-    this.name = name;
-    this.done = done;
-    this.inProgress = inProgress;
-    this.waiting = waiting;
+    this.setState({
+      userId, name, done, inProgress, waiting
+    });
   }
 
   render() {
     return (
-      <div>
+      <div key={this.state.userId}>
         <h5 className="mb-0">
-          <a className="text-dark" href="/">Hello, {this.name}</a>
+          <a className="text-dark" href="/">Hello, {this.state.name}</a>
         </h5>
-        <div>You have <strong>{this.done + this.inProgress + this.waiting}</strong> tasks</div>
-        <div>Done: <strong>{this.done}</strong></div>
-        <div>In progress: <strong>{this.inProgress}</strong></div>
-        <div>Waiting: <strong>{this.waiting}</strong></div>
+        <div>You have <strong>{this.state.done + this.state.inProgress + this.state.waiting}</strong> tasks</div>
+        <div>Done: <strong>{this.state.done}</strong></div>
+        <div>In progress: <strong>{this.state.inProgress}</strong></div>
+        <div>Waiting: <strong>{this.state.waiting}</strong></div>
         <a href="/">Go to the task list</a>
       </div>
     );
