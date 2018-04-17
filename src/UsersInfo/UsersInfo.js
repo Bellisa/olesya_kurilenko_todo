5cb1bfe6720afd9f496219f16364fc7985069347
Olesya Kurilenko <olesya.kurilenko@gmail.com>
2018-04-17 16:31:43 +0200

    hw20 -del prom -getImgsUrl

--- src/UsersInfo/UsersInfo.js
+++ src/UsersInfo/UsersInfo.js
@@ -12,55 +12,60 @@ export class UsersInfo extends Component {
     };
   }
 
-  getImagesUrl = path => new Promise((resolve) => {
+  // getImagesUrl = path => new Promise((resolve) => {
+  // const massurl = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg'];
+  // fs.readdir(path, (err, items) => {
+  // console.log(massurl.map(img => `${path}/${img}`));
+  // const collecttions = items.filter(img => this.regFiles.test(img));
+  //  resolve(massurl.map(img => `${path}/${img}`));
+  // });
+
+  getImagesUrl = (path) => {
     const massurl = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg'];
-    // fs.readdir(path, (err, items) => {
-    console.log(massurl.map(img => `${path}/${img}`));
-    // const collecttions = items.filter(img => this.regFiles.test(img));
-    resolve(massurl.map(img => `${path}/${img}`));
+    return (massurl.map(img => `${path}/${img}`));
+  }
+
+  getUsers = url => new Promise((resolve, reject) => {
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        result => resolve(result.map(el => ({ id: el.id, name: el.name, email: el.email })))
+        ,
+        (error) => {
+          reject(error);
+        }
+      );
   });
 
-getUsers = url => new Promise((resolve, reject) => {
-  fetch(url)
-    .then(res => res.json())
-    .then(
-      result => resolve(result.map(el => ({ id: el.id, name: el.name, email: el.email })))
-      ,
-      (error) => {
-        reject(error);
-      }
+  componentDidMount() {
+    this.getUsers('https://jsonplaceholder.typicode.com/users')
+      .then(text => this.setState({ users: text }))
+      .catch(console.log);
+
+    this.getImagesUrl('')
+      .then(text => this.setState({ imagesUrl: text }))
+      .catch(console.log);
+  }
+
+  render() {
+    return (
+      <Tabs>
+        <Tab
+          key={0}
+          title="Users List"
+        >
+          <h1>Users List</h1>
+          <ComUserList users={this.state.users} />
+        </Tab>
+        <Tab
+          key={1}
+          title="Gallery"
+        ><h1>Gallery</h1>
+          <ComGallary imagesUrl={this.state.imagesUrl} />
+
+        </Tab>
+      </Tabs >
     );
-});
-
-componentDidMount() {
-  this.getUsers('https://jsonplaceholder.typicode.com/users')
-    .then(text => this.setState({ users: text }))
-    .catch(console.log);
-
-  this.getImagesUrl('')
-    .then(text => this.setState({ imagesUrl: text }))
-    .catch(console.log);
-}
-
-render() {
-  return (
-    <Tabs>
-      <Tab
-        key={0}
-        title="Users List"
-      >
-        <h1>Users List</h1>
-        <ComUserList users={this.state.users} />
-      </Tab>
-      <Tab
-        key={1}
-        title="Gallery"
-      ><h1>Gallery</h1>
-        <ComGallary imagesUrl={this.state.imagesUrl} />
-
-      </Tab>
-    </Tabs >
-  );
-}
+  }
 }
 
